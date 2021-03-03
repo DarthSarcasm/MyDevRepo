b00f7feeb6204b34915c85f13ceacf51018df1bb
Antony Harris <antonyharris@Triskelion.local>
2021-03-03 09:00:09 +0000

    Git push to rebaseline#

--- Pi Files/server_reboot.sh
+++
@@ -1,5 +0,0 @@
-# Quick and dirty script to unmount the drive before a reboot...
-
-sudo umount /dev/sda2
-sleep 10
-sudo reboot
--- Pi Files/server_shutdown.sh
+++
@@ -1,5 +0,0 @@
-# Quick and dirty script to unmount the drive before a shutdown...
-
-sudo umount /dev/sda2
-sleep 10
-sudo shutdown -P -h 0
