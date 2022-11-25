e0c90fc3597edcd1d6fe36f177b86cc428765630
DarthSarcasm <tony@harris.net>
2022-11-25 09:47:19 +0000

    Random changes

--- test.c
+++ test.c
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 # Basic Hello world, done PROPERLY!
 
 void main()	{
 	printf("Hello World!");
 }
-=======
-void main()	{
-	printf("Hello World!");
-}
->>>>>>> 1d7e952... Initial Internal Commit
