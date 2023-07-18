bb56a5b4198dd6a6c3ecc6fb350003396687ca47
DarthSarcasm <tony@harris.net>
2023-07-18 03:24:09 +0100

    Modifications to sd card scripts

--- Unix/sd_image.sh
+++ Unix/sd_image.sh
@@ -13,7 +13,7 @@ diskutil list
 
 # unmount the SDC ready for copying
 unset disk_id
-read -p "Which disk to unmount (/dev/...):" disk_id || error_exit "Error entering Disk ID"
+read -p "Which disk to image (/dev/...):" disk_id || error_exit "Error entering Disk ID"
 [ ! -z "$disk_id" ] && echo "Disk to unmount - /dev/$disk_id" || error_exit "Entry is empty"
 
 # dump the SDC data into an img file
@@ -21,17 +21,12 @@ unset image_name
 read -p "Please enter image name to create in Downloads folder (include extension):" image_name || error_exit "Error entering image name"
 [ ! -z "$image_name" ] && echo "Image to create - $image_name" || error_exit "Entry is empty"
 
-# compress the final image?
-unset compression
-read -p "Compress the final image? Y/[N]:" compression || error_exit "Error entering image name"
-[ ! -z "$compression" ] && echo "Compression enabled? - $compression" || error_exit "Entry is empty"
-
 # After final confirmation, do the things!
 unset confirm
-read -p "Create $image_name from /dev/$disk_id? [Y]/N:" confirm
+read -p "Write $image_name to /dev/$disk_id? [Y]/N:" confirm
 if [ "$confirm" = "Y" ] || [ "$confirm" = "y" ] || [ "$confirm" = "" ]
 then
-	echo "Creating image $image_name from /dev/$disk_id. Please wait..."
+	echo "Writing image $image_name to /dev/$disk_id. Please wait..."
 else
 	echo "Doing nothing!"
 	exit 0
@@ -39,17 +34,8 @@ fi
 
 # DO THE THING!
 diskutil unmountDisk /dev/$disk_id || error_exit "Error unounting disk"
-sudo dd if=/dev/$disk_id of=$HOME/Downloads/$image_name || error_exit "Error creating image"
-
-# Compress the final image, if set.
-if [ "$compression" = "Y" ] || [ "$compression" = "y" ]
-then
-	echo "Compressing $image_name - Please Wait"
-	zip -X $HOME/Downloads/$image_name.zip $HOME/Downloads/$image_name || error_exit "Unable to compress image"
-	echo "Compression complete"
-	# Delete the original image now it's compressed
-	rm $HOME/Downloads/$image_name
-fi
+#sudo dd if=/dev/$disk_id of=$HOME/Downloads/$image_name || error_exit "Error creating image"
+sudo dd if=$HOME/Downloads/$image_name of=/dev/$disk_id || error_exit "Error writing image"
 
 # Did the thing, exit now.
 echo "Image created successfully!"
