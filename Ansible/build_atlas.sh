d1f540bb85b856b5c1d65ee9bf1eafcb690e153b
Antony Harris <tony@harris.net>
2023-12-22 14:57:07 +0000

    More stages of server build set up

---
+++ Ansible/build_atlas.sh
@@ -0,0 +1,20 @@
+# Script to update / build atlaspi using ansible where possible.
+
+ANSIBLE_INVENTORY='/Volumes/T7/*Projects/Code/MyDevRepo/MyDevRepo/Ansible/inventory/inventory.yml'
+ANSIBLE_PLAYBOOKS='/Volumes/T7/*Projects/Code/MyDevRepo/MyDevRepo/Ansible/playbooks'
+UNIX_SCRIPT_PATH='/Volumes/T7/*Projects/Code/MyDevRepo/MyDevRepo/Unix/Pi_Files'
+
+# Update the base image
+ansible-playbook -i $ANSIBLE_INVENTORY -e "hosts=rpi_build" $ANSIBLE_PLAYBOOKS/update_rpi.yml
+
+# Basic rpi build steps
+ansible-playbook -i $ANSIBLE_INVENTORY -e "scripts='$UNIX_SCRIPT_PATH/General'" $ANSIBLE_PLAYBOOKS/base_rpi.yml
+
+# Configure zsh shell
+ansible-playbook -i $ANSIBLE_INVENTORY -e "scripts='$UNIX_SCRIPT_PATH/General'" $ANSIBLE_PLAYBOOKS/configure_zsh.yml
+
+# That's the default stuff done - now on to the tricky, Docker & NAS config...
+
+# Do the Atlas Stuff
+# ansible-playbook -i $ANSIBLE_INVENTORY -e "scripts='$UNIX_SCRIPT_PATH/Atlas'" $ANSIBLE_PLAYBOOKS/configure_nas.yml
+# ansible-playbook -i $ANSIBLE_INVENTORY $ANSIBLE_PLAYBOOKS/configure_docker.yml
--- Ansible/build_rpi.sh
+++ Ansible/build_rpi.sh
@@ -5,7 +5,7 @@ ANSIBLE_PLAYBOOKS='/Volumes/T7/*Projects/Code/MyDevRepo/MyDevRepo/Ansible/playbo
 UNIX_SCRIPT_PATH='/Volumes/T7/*Projects/Code/MyDevRepo/MyDevRepo/Unix/Pi_Files'
 
 # Update the base image
-ansible-playbook -i $ANSIBLE_INVENTORY $ANSIBLE_PLAYBOOKS/update_rpi.yml
+ansible-playbook -i $ANSIBLE_INVENTORY -e "hosts=rpi_build" $ANSIBLE_PLAYBOOKS/update_rpi.yml
 
 # Basic rpi build steps
 ansible-playbook -i $ANSIBLE_INVENTORY -e "scripts='$UNIX_SCRIPT_PATH/General'" $ANSIBLE_PLAYBOOKS/base_rpi.yml
@@ -13,4 +13,4 @@ ansible-playbook -i $ANSIBLE_INVENTORY -e "scripts='$UNIX_SCRIPT_PATH/General'" 
 # Configure zsh shell
 ansible-playbook -i $ANSIBLE_INVENTORY -e "scripts='$UNIX_SCRIPT_PATH/General'" $ANSIBLE_PLAYBOOKS/configure_zsh.yml
 
-# That's the defualt stuff done!
+# That's the default stuff done!
