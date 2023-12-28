97068a78366d2019392579cae6a89ccda311888f
Antony Harris <tony@harris.net>
2023-12-28 23:32:41 +0000

    misc changes

--- Ansible/build_rpi.sh
+++ Ansible/build_rpi.sh
@@ -1,12 +1,12 @@
 # Script to update / build atlaspi using ansible where possible.
 
-REPO_DIR='/Volumes/T7/*Projects/Code/MyDevRepo/MyDevRepo'
+REPO_DIR='/Users/antonyharris/Code/MyDevRepo'
 ANSIBLE_INVENTORY=$REPO_DIR/Ansible/inventory/inventory.yml
 ANSIBLE_PLAYBOOKS=$REPO_DIR/Ansible/playbooks
 UNIX_SCRIPT_PATH=$REPO_DIR/Unix/Pi_Files
 
 # Update the base image
-ansible-playbook -i $ANSIBLE_INVENTORY -e "hosts=rpi_build" $ANSIBLE_PLAYBOOKS/update_rpi.yml
+ansible-playbook -i $ANSIBLE_INVENTORY -e "nodes=rpi_build" $ANSIBLE_PLAYBOOKS/update_rpi.yml
 
 # Basic rpi build steps
 ansible-playbook -i $ANSIBLE_INVENTORY -e "scripts='$UNIX_SCRIPT_PATH/General'" $ANSIBLE_PLAYBOOKS/base_rpi.yml
@@ -14,4 +14,4 @@ ansible-playbook -i $ANSIBLE_INVENTORY -e "scripts='$UNIX_SCRIPT_PATH/General'" 
 # Configure zsh shell
 ansible-playbook -i $ANSIBLE_INVENTORY -e "scripts='$UNIX_SCRIPT_PATH/General'" $ANSIBLE_PLAYBOOKS/configure_zsh.yml
 
-# That's the default stuff done!
+# That's the default stuff done
