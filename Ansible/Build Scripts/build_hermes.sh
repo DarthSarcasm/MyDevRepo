8e1a1e2fd133c35d05e2e0b83292949a00a60d51
Antony Harris <tony@harris.net>
2024-02-21 08:31:49 +0000

    New script

---
+++ Ansible/Build Scripts/build_hermes.sh
@@ -0,0 +1,19 @@
+# Script to update / build atlaspi using ansible where possible.
+
+REPO_DIR='/Volumes/T7/*Projects/Code/MyDevRepo/MyDevRepo'
+ANSIBLE_INVENTORY=$REPO_DIR/Ansible/inventory/inventory.yml
+ANSIBLE_PLAYBOOKS=$REPO_DIR/Ansible/playbooks
+UNIX_SCRIPT_PATH=$REPO_DIR/Unix/Pi_Files
+COMPOSE_PATH=$REPO_DIR/Docker
+
+# Update the base image
+ansible-playbook -i $ANSIBLE_INVENTORY -e "nodes=rpi_build" $ANSIBLE_PLAYBOOKS/update_rpi.yml
+
+# Basic rpi build steps
+ansible-playbook -i $ANSIBLE_INVENTORY -e "scripts='$UNIX_SCRIPT_PATH/General'" $ANSIBLE_PLAYBOOKS/base_rpi.yml
+
+# Configure zsh shell
+ansible-playbook -i $ANSIBLE_INVENTORY -e "scripts='$UNIX_SCRIPT_PATH/General'" $ANSIBLE_PLAYBOOKS/configure_zsh.yml
+
+# Install and configure Docker
+ansible-playbook -i $ANSIBLE_INVENTORY $ANSIBLE_PLAYBOOKS/configure_docker.yml
